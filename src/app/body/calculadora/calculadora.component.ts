49544289ecadba5d86d0ac1fca75129224719fba
ElvisStwar <elvis.stwar@hotmail.com>
2022-12-27 12:57:11 -0500

    Creacion de calculadora

--- src/app/body/calculadora/calculadora.component.ts
+++ src/app/body/calculadora/calculadora.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+  res= 0 ;
+  res2="0"
+  suma(num1:string, num2:string){
+    this.res =  parseFloat(num1)+parseFloat(num2);
+    this.res2 = this.res.toString()
+    if(num1.length<1 || num2.length<1){
+      this.res2 = "Ingrese los datos."
+    }
+  }
+
+  resta(num1:string, num2:string){
+    this.res =  parseFloat(num1)-parseFloat(num2);
+    this.res2 = this.res.toString()
+    if(num1.length<1 || num2.length<1){
+      this.res2 = "Ingrese los datos."
+    }
+  }
+
+  multi(num1:string, num2:string){
+    this.res =  parseFloat(num1)*parseFloat(num2);
+    this.res2 = this.res.toString()
+    if(num1.length<1 || num2.length<1){
+      this.res2 = "Ingrese los datos."
+    }
+  }
+
+  divi(num1:string, num2:string){
+    let n1 = parseFloat(num1)
+    let n2 = parseFloat(num2)
+    this.res =  n1/n2;
+    this.res2 = this.res.toString()
+    if(num1.length<1 || num2.length<1){
+      this.res2 = "Ingrese los datos."
+    } else if(n1==0 && n2==0){
+      this.res2 = "Infinito?"
+    }
+  }
 
 }
